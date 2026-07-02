ed3db766a303b664c78fcfda2d340749b8154d3a
miladhzz <miladhzz@gmail.com>
2026-07-02 17:30:25 +0330

    add about

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { LocationCard } from "../location-card/location-card";
 import { LocationCardService } from "../services/location-card.service";
 import { LocationCardDto } from '../models/location-card.dto';
+import { About } from "../about/about";
 
 @Component({
   selector: 'app-home',
-  imports: [LocationCard],
+  imports: [LocationCard, About],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
