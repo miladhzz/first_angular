fb8423c17d15a2edffc7b383fb449f0fb7c1e522
miladhzz <miladhzz@gmail.com>
2026-07-03 01:58:18 +0330

    submit form

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LocationCardService } from '../services/location-card.service';
 import { LocationCardDto } from '../models/location-card.dto';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
@@ -15,9 +16,23 @@ export class Details {
   locationCardService = inject(LocationCardService);
   locationCard: LocationCardDto | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     const locationcardId = Number(this.route.snapshot.params['id']);
     this.locationCard = this.locationCardService.getLocationById(locationcardId);
+  }
+
+  submitApplication() {
+    this.locationCardService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 
 }
