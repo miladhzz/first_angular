344641d8e73dbf360abe1669de086c01bec016ca
miladhzz <miladhzz@gmail.com>
2026-07-04 02:21:44 +0330

    init add photo

--- api/api/models.py
+++ api/api/models.py
@@ -5,7 +5,7 @@ class Location(models.Model):
     name = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    photo = models.URLField(max_length=500)
+    photo = models.ImageField(upload_to='locations/')
     available_units = models.PositiveIntegerField()
     wifi = models.BooleanField(default=False)
     laundry = models.BooleanField(default=False)
